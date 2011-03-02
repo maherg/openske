fb328ef7219a75f8ce66d12caa94538cfb39f84a
Maher Gamal <mahergamal@gmail.com>
2011-03-02 16:43:41 +0200

    Added proogy/200/500/1000 infrastructures.

---
+++ openske-expertise/src/main/resources/com/openske/infrastructures/Infrastructure200.groovy
@@ -0,0 +1,31 @@
+import com.openske.model.hardware.*
+import com.openske.model.software.*
+import com.openske.model.security.*
+
+def router = new Router("router")
+session.insert(router)
+
+// Hosts and Software
+200.times { hostCount ->
+    def host = new Host("host-${hostCount}")
+    router.addConnection(host)
+    session.insert(host)
+    // Software
+    100.times { softCount ->
+        def soft = new Software("vendor-${softCount}", "software-${softCount}", softCount.toString(), host)
+        soft.addWeakness("CWE-285").addWeakness("CWE-400").addWeakness("CWE-707")
+        session.insert(soft)
+    }
+}
+
+// Users
+20.times { userCount ->
+    def user = new User("User ${userCount}", "user_${userCount}@user.com", router.getConnections().get(userCount), false)
+    session.insert(user)
+}
+
+// Attackers
+1.times { userCount ->
+    def user = new User("Attacker ${userCount}", "attacker_${userCount}@attacker.com",router.getConnections().get(userCount), true)
+    session.insert(user)
+}
